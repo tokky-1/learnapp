26976579e43e8023d321111f3e5f179892775884
tokky-1 <144350428+tokky-1@users.noreply.github.com>
2025-07-14 12:35:04 +0100

    Update connector.py

--- database/connector.py
+++ database/connector.py
@@ -5,8 +5,7 @@ import os
 
 load_dotenv(override=True)
 
-#SQLALCHEMY_DB_URL =os.getenv("SQLALCHEMY_DB_URL")
-SQLALCHEMY_DB_URL = "postgresql://neondb_owner:npg_w1U4YQJitMKc@ep-restless-art-aem19nqy-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require"
+SQLALCHEMY_DB_URL =os.getenv("SQLALCHEMY_DB_URL")
 engine = create_engine(SQLALCHEMY_DB_URL)
 #print(SQLALCHEMY_DB_URL)
 sessionlocal = sessionmaker(bind = engine)
