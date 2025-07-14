6d09849dd2bda9617cbc18d13de7dd6f1eda53dd
tokky-1 <144350428+tokky-1@users.noreply.github.com>
2025-07-14 12:15:21 +0100

    adds neon DB link

--- database/connector.py
+++ database/connector.py
@@ -5,9 +5,10 @@ import os
 
 load_dotenv(override=True)
 
-SQLALCHEMY_DB_URL =os.getenv("SQLALCHEMY_DB_URL")
+#SQLALCHEMY_DB_URL =os.getenv("SQLALCHEMY_DB_URL")
+SQLALCHEMY_DB_URL = "postgresql://neondb_owner:npg_w1U4YQJitMKc@ep-restless-art-aem19nqy-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require"
 engine = create_engine(SQLALCHEMY_DB_URL)
-print(SQLALCHEMY_DB_URL)
+#print(SQLALCHEMY_DB_URL)
 sessionlocal = sessionmaker(bind = engine)
 
 def get_db():
